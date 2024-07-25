9eba521accaa50068e6fb309459225bac8489a0f
sah alom <sahalom4729@gmail.com>
2024-07-25 21:10:52 +0400

    added login code

--- Controller/users.controller.js
+++ Controller/users.controller.js
@@ -1,26 +1,95 @@
 const { db } = require("../utils/DB-connect");
 const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 
 
 const usersCollection = db.collection('users')
 
+
+// Register  
+
+
 const addUser = async (req, res) => {
     try {
-        const userData = req.body;
-        const hashedPassword = await bcrypt.hash(userData.password, 10)
-        const result = await usersCollection.insertOne({
-            ...userData,
-            password: hashedPassword 
-        });
-        res.status(201).send(result);
+      const userData = req.body;
+      const existingUser = await usersCollection.findOne({ email: userData.email });
+  
+      if (existingUser) {
+        console.log('aceeee');
+        return res.send({ message: 'Account with this email already exists' });
+      }
+  
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+      const result = await usersCollection.insertOne({
+        ...userData,
+        password: hashedPassword
+      });
+  console.log('in');
+      res.status(201).send(result);
     } catch (error) {
-        console.error('Error adding user:', error);
-        res.status(500).send('Internal Server Error');
+      console.error('Error adding user:', error);
+      res.status(500).send('Internal Server Error');
     }
-};
+  };
+
+
+// User  Login 
+
+const login =async(req,res)=>{
+
+    const { email, password } = req.body;
+    try {
+      const user = await usersCollection.findOne({ email: email });
+
+      if (!user) {
+        return res.send({ message: 'Invalid email' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.send({ message: 'Invalid  password' });
+      }
+      res.send({ message: 'Login successful', user });
+
+
+    } catch (error) {
+      res.sendStatus(500); // Internal server error
+    }
+
+
+
+}
+
+
+// check user login 
+const isLogin = async (req, res) => {
+    try {
+      const token = req.body.token;
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN);
+      const email = decoded.data;
+  
+      const existingUser = await usersCollection.findOne({ email: email });
+  
+      if (existingUser) {
+        const { password, ...userWithoutPassword } = existingUser;
+  
+        res.send({ message: 'User is logged in', user: userWithoutPassword });
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      res.sendStatus(500);
+    }
+  }
+
+
 
 
 module.exports={
     addUser,
+    login,
+    isLogin,
 }
--- Routes/users.routes.js
+++ Routes/users.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router= express.Router()
 
-const { addUser,} = require('../Controller/users.controller');
+const { addUser, isLogin, login,} = require('../Controller/users.controller');
 
 //  example=== /user/addUser
 
 router.post('/addUser',addUser);
+router.post('/login',login);
+router.post('/is-login',isLogin);
 
 
 
