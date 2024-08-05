b6ede97277f4e6e90102ffab54a977205d11e2ff
sah alom <sahalom4729@gmail.com>
2024-08-05 20:58:26 +0400

    added AddBloodModal.propTypes = {

--- Controller/bloodBank.controller.js
+++ Controller/bloodBank.controller.js
@@ -10,8 +10,52 @@ const addBloodDonor =async (req,res)=>{
     res.send(result)
 }
 
+const getBloodGroupSummary = async (req, res) => {
+    try {
+        const result = await bloodBankCollection.aggregate([
+            { $match: { status: 'Available' } },
+            {
+                $group: {
+                    _id: "$bloodGroup",
+                    totalDonors: {
+                        $sum: {
+                            $cond: [{ $eq: ["$type", "donor"] }, 1, 0]
+                        }
+                    },
+                    totalBloodBags: {
+                        $sum: {
+                            $cond: [{ $eq: ["$type", "blood"] }, 1, 0]
+                        }
+                    }
+                }
+            },
+            {
+                $addFields: {
+                    totalAvailable: { $sum: ["$totalDonors", "$totalBloodBags"] }
+                }
+            },
+            {
+                $project: {
+                    bloodGroup: "$_id",
+                    totalDonors: 1,
+                    totalBloodBags: 1,
+                    totalAvailable: 1,
+                    _id: 0
+                }
+            }
+        ]).toArray();
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ message: 'Error fetching blood group summary', error });
+    }
+}
+
+
+
+
 
 module.exports={
 
     addBloodDonor,
+    getBloodGroupSummary,
 }
--- Routes/bloodBank.router.js
+++ Routes/bloodBank.router.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { addBloodDonor } = require('../Controller/bloodBank.controller');
 const router= express.Router()
 
+const { addBloodDonor, getBloodGroupSummary } = require('../Controller/bloodBank.controller');
+
 router.post('/addBloodDonor',addBloodDonor);
+router.get('/blood-summary',getBloodGroupSummary);
 
 module.exports=router;
